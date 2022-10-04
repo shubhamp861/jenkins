9d98f59ee2c1bcd7bd09794f1da18578255be9df
shubhamp861 <46345008+shubhamp861@users.noreply.github.com>
2022-10-05 00:54:55 +0530

    Update JenkinsApplicationTests.java

--- src/test/java/com/example/jenkins/JenkinsApplicationTests.java
+++ src/test/java/com/example/jenkins/JenkinsApplicationTests.java
@@ -2,12 +2,17 @@ package com.example.jenkins;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class JenkinsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assert.hasText("Tes","Tes");
 	}
-
+	@Test
+	void contextLoadsooooooo() {
+		Assert.hasText("Tes","Tesx");
+	}
 }
